50445ccf9c703ec6fc63b394b235faa391b2f6f0
Kiyoshi Sugimura <sgk03401jp@gmail.com>
2015-05-24 09:52:35 +0900

    Signed-off-by: Kiyoshi Sugimura <sgk03401jp@gmail.com>

--- main.c
+++ main.c
@@ -16,6 +16,21 @@ __CONFIG(
         & HS            // 外部ハイスピード発振子　EXTRC Oscillator, RC on RA7/OSC1/CLKIN
         );
 
+void interrupt T1Handler(void) { //100msec毎の割り込み
+	int flag1;
+	TMR1IF = 0; // 割り込みフラグクリア
+        TMR1H = 11;                             //
+        TMR1L = 220;
+	if(flag1 == 0) {
+		RB4 = 1;
+		flag1 == 1;
+	} else {
+		RB4 = 0;
+		flag1 = 0;
+	}
+
+}
+
 void main()
 {
 	char ch;
@@ -31,8 +46,17 @@ void main()
 	TRISB = 0x00; 			// PORTB as Output
 //	TRISD = 0xff;			// PORTD as Input
 
+        //タイマ１初期設定
+        //T1CON レジスタ(Timer1 Control Resister)
+        T1CKPS1 = 1;    //プリスケーラの設定　T1CKPS0とｾｯﾄで　1/8のプリスケーラに設定
+        T1CKPS0 = 1;    //11:1/8  10:1/4 01:1/2 00:1/1
+        T1OSCEN = 1;    //外部専用発振器動作
+        T1SYNC = 0;             //外部入力の内部クロックへの同期
+        TMR1CS = 1;             //内部クロック（Fosc/4) // 1:外部クロックまたは発振回路
+        TMR1ON = 1;             //タイマ１を作動させる　//　0：作動させない
 
-	OPTION = 0b00000111;	//プリスケール設定 1/256,ポートB弱プルアップ設定 **2.00
+
+//	OPTION = 0b00000111;	//プリスケール設定 1/256,ポートB弱プルアップ設定 **2.00
 //	GIE  = 0;				//割り込みは使用しない
 
 	RB4 = 1;
@@ -41,6 +65,22 @@ void main()
 	UART_Init(9600);
 	cnt = PORTA & 0x0f;
 	RB7=0;
+
+//Clock：　20MHｚ/4　→　5MHz　0.2μsec
+//　100msec毎の割り込みを行う
+//プリスケーラ　1/8 //Prescale Select bits
+
+//    (65536 -N)*0.2*8 = 100000
+//     N = 65536 - 100000 /8/0.2  = 3036
+//     3036/256 = 11.8593
+//     3036 -256*11 = 220
+//     59286 ÷　256　＝　11　余り220
+        TMR1H = 11;                             //
+        TMR1L = 220;
+
+        TMR1IE = 1;                             // タイマ1割り込み許可
+        PEIE = 1;                               // 周辺割り込み許可許可
+        GIE = 1;                                // グローバル割り込み許可
 
 	__delay_ms(500);
 	UART_Write_Text("PE0123456789");
